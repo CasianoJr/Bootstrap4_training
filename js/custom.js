449846790c3415d374c4ebfedc7900497cddf721
Casiano <casianojrfernandez@gmail.com>
2020-04-27 14:32:14 +0800

    update

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+$(function () {
+  $("#inputOut").datepicker({
+    changeMonth: true,
+    changeYear: true,
+    showButtonPanel: true,
+  });
+  $("#inputIn").datepicker({
+    changeMonth: true,
+    changeYear: true,
+    showButtonPanel: true,
+  });
+});
